ae40df17bb0a790889e59d7078bd9ee320115d07
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-12-27 04:17:34 -0300

    post no form

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,32 +1,85 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 import "./styles.css";
 
-const Form = () => {
+const Form: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    nome: "",
+    idade: 0,
+    linguagemId: 1,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: name === "idade" ? parseInt(value, 10) : value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8081/pesquisa", formData);
+      // Após o sucesso do cadastro, redirecione para a rota /registros
+      navigate("/registros");
+    } catch (error) {
+      // Handle error, e.g., show an error message
+      console.error("Erro ao cadastrar pesquisa:", error);
+    }
+  };
+
   return (
     <>
       <div className="form-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h2>Cadastre uma nova pesquisa</h2>
           <div className="form-group">
             <label htmlFor="nome">Digite seu nome</label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              name="nome"
+              value={formData.nome}
+              onChange={handleChange}
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="nome">Qual é a sua idade</label>
-            <input type="number" className="form-control" />
+            <label htmlFor="idade">Qual é a sua idade</label>
+            <input
+              type="number"
+              className="form-control"
+              name="idade"
+              value={formData.idade}
+              onChange={handleChange}
+            />
           </div>
 
           <div className="form-group">
-            <label htmlFor="exampleFormControlSelect1">Qual é a sua linguagem favorita</label>
-            <select className="form-control" id="exampleFormControlSelect1">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
+            <label htmlFor="linguagemId">Qual é a sua linguagem favorita</label>
+            <select
+              className="form-control"
+              id="exampleFormControlSelect1"
+              name="linguagemId"
+              value={formData.linguagemId}
+              onChange={handleChange}
+            >
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
             </select>
           </div>
 
-          <button className="btn-submit">Cadastrar</button>
+          <button type="submit" className="btn-submit">
+            Cadastrar
+          </button>
         </form>
       </div>
     </>
